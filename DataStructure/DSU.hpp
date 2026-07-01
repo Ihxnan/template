a73b0d627b175296cdc9e1553cdd5427e7f313f1
ihxnan <1377537238@qq.com>
2026-07-01 10:24:25 +0800

    main: update

--- DataStructure/DSU.hpp
+++ DataStructure/DSU.hpp
@@ -1,35 +1,23 @@
-struct DSU
-{
-    vector<int> fa, sz;
+struct DSU {
+  vector<int> fa, sz;
 
-    DSU(int n) : fa(n + 1), sz(n + 1, 1)
-    {
-        iota(fa.begin(), fa.end(), 0);
-    }
+  DSU(int n) : fa(n + 1), sz(n + 1, 1) { iota(fa.begin(), fa.end(), 0); }
 
-    int get(int x)
-    {
-        while (x != fa[x])
-            x = fa[x] = fa[fa[x]];
-        return x;
-    }
+  int get(int x) {
+    while (x != fa[x])
+      x = fa[x] = fa[fa[x]];
+    return x;
+  }
 
-    bool merge(int x, int y)
-    {
-        if ((x = get(x)) == (y = get(y)))
-            return false;
-        fa[y] = x;
-        sz[x] += sz[y];
-        return true;
-    }
+  bool merge(int x, int y) {
+    if ((x = get(x)) == (y = get(y)))
+      return false;
+    fa[y] = x;
+    sz[x] += sz[y];
+    return true;
+  }
 
-    bool same(int x, int y)
-    {
-        return get(x) == get(y);
-    }
+  bool same(int x, int y) { return get(x) == get(y); }
 
-    int size(int x)
-    {
-        return sz[get(x)];
-    }
+  int size(int x) { return sz[get(x)]; }
 };
--- DataStructure/Fenwick.hpp
+++ DataStructure/Fenwick.hpp
@@ -1,38 +1,29 @@
-template <typename T> struct Fenwick
-{
-    int n;
-    vector<T> tree;
+template <typename T> struct Fenwick {
+  int n;
+  vector<T> tree;
 
-    Fenwick(int n) : n(n), tree(n + 1)
-    {
-    }
+  Fenwick(int n) : n(n), tree(n + 1) {}
 
-    void add(int x, const T &v)
-    {
-        for (; x <= n; x += x & -x)
-            tree[x] += v;
-    }
+  void add(int x, const T &v) {
+    for (; x <= n; x += x & -x)
+      tree[x] += v;
+  }
 
-    T query(int x)
-    {
-        T ans{};
-        for (; x > 0; x -= x & -x)
-            ans += tree[x];
-        return ans;
-    }
+  T query(int x) {
+    T ans{};
+    for (; x > 0; x -= x & -x)
+      ans += tree[x];
+    return ans;
+  }
 
-    T query(int l, int r)
-    {
-        return query(r) - query(l - 1);
-    }
+  T query(int l, int r) { return query(r) - query(l - 1); }
 
-    int select(const T &k)
-    {
-        int x = 0;
-        T cur{};
-        for (int i = 1 << __lg(n); i; i >>= 1)
-            if (x + i <= n && cur + tree[x + i] <= k)
-                cur += tree[x += i];
-        return x;
-    }
+  int select(const T &k) {
+    int x = 0;
+    T cur{};
+    for (int i = 1 << __lg(n); i; i >>= 1)
+      if (x + i <= n && cur + tree[x + i] <= k)
+        cur += tree[x += i];
+    return x;
+  }
 };
